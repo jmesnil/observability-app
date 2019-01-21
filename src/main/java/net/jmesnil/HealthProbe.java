f1b81d77b586218f0ff6d6b1b6d99b7f60c5d71c
Jeff Mesnil <jmesnil@gmail.com>
2019-01-21 15:29:55 +0100

    Initial Commit

---
+++ src/main/java/net/jmesnil/HealthProbe.java
@@ -0,0 +1,35 @@
+package net.jmesnil;
+
+import java.util.Random;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
+
+
+@Health
+public class HealthProbe implements HealthCheck {
+
+    @Inject
+    @ConfigProperty(name="app.health.ratio", defaultValue = "0.8")
+    private double healthRatio;
+
+    private final Random random = new Random();
+
+    @Override
+    public HealthCheckResponse call() {
+        HealthCheckResponseBuilder check = HealthCheckResponse.named("app.healthCheck");
+        double val = random.nextDouble();
+        if (val <= healthRatio) {
+            return check
+                    .withData("failure", Double.toString(val))
+                    .withData("ratio", Double.toString(healthRatio))
+                    .down().build();
+        }
+        return check.up().build();
+    }
+}
---
+++ src/main/java/net/jmesnil/HelloService.java
@@ -0,0 +1,40 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.jmesnil;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.opentracing.Traced;
+
+
+@Stateless
+@Traced
+@Counted
+public class HelloService {
+
+    @Inject
+    @ConfigProperty(name="app.hello", defaultValue = "Hello")
+    private String hello;
+
+    public String createHelloMessage(String name) {
+        return hello + " " + name + "!";
+    }
+
+}
